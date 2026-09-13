300491b9a393558285c94c16fe41b30cb1d43dcb
Genome Corporation <rigueti.arthur12@gmail.com>
2026-09-12 23:36:39 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
+
+import {
+  initializeAppCheck,
+  ReCaptchaEnterpriseProvider
+} from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app-check.js";
+
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyAbg5FjbC9czcifprfxmsRWeYttXL54uho",
@@ -10,8 +19,32 @@ const firebaseConfig = {
   appId: "1:987270159575:web:ffa31f2d786e039e2ba492"
 };
 
+
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
+
+// ==========================================
+// FIREBASE APP CHECK + reCAPTCHA ENTERPRISE
+// ==========================================
+
+const appCheck = initializeAppCheck(app, {
+
+  provider: new ReCaptchaEnterpriseProvider(
+    "6Lc8mbgtAAAAAEeqgzzg9FK_fK-Foql9QGwg4v8r"
+  ),
+
+  isTokenAutoRefreshEnabled: true
+
+});
+
+
+// Authentication
 const auth = getAuth(app);
 
-export { app, auth };
+
+export {
+  app,
+  auth,
+  appCheck
+};
